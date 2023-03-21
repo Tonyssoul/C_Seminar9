1724c0004d29ff288426ea51417c942b551750e9
Anton Kargapolov <igloooooook@gmail.com>
2023-03-21 16:13:55 +0500

    dz task 066

---
+++ dz_task_066/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int numM, int numN, int sum)
+{
+    if (numM < numN)
+    {
+        sum += numM;
+        if (numM == numN)
+        {
+            return sum;
+        }
+        numM++;
+        return Sum(numM, numN, sum);
+    }
+    else
+    {
+        sum += numN;
+        if (numM == numN)
+        {
+            return sum;
+        }
+        numN++;
+        return Sum(numM, numN, sum);
+    }
+}
+
+
+int m = ReadInt("Введите число M ");
+int n = ReadInt("Введите число N ");
+System.Console.WriteLine(Sum(m, n, 0));
