a8c63d897c888b263d7b14305f8c25668b521bae
Anton Kargapolov <igloooooook@gmail.com>
2023-03-21 15:26:57 +0500

    task 067

---
+++ task_067/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int num, int sum)
+{
+    sum += num % 10;
+    if (num / 10 == 0)
+    {
+        return sum;
+    }
+    return Sum(num / 10, sum);
+}
+
+
+int m = ReadInt("Введите число ");
+System.Console.WriteLine(Sum(m, 0));
